83fbfc4fb016b5123620edd9f4ced072c07c2c98
tharunikaaprabhu <125335009+tharunikaaprabhu@users.noreply.github.com>
2024-08-11 07:06:27 +0530

    day 8

---
+++ Day 8/src/pages/Web/Login.jsx
@@ -0,0 +1,82 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Button } from "@/components/ui/button";
+import { Link } from 'react-router-dom';
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import toast, { Toaster } from 'react-hot-toast';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    // Replace this with your actual authentication logic
+    if (username === 'admin' && password === 'admin') {
+      toast.success('Admin login successful!');
+      navigate('/admin/dashboard');
+    } else if (username === 'user' && password === 'user') {
+      toast.success('User login successful!');
+      navigate('/user/dashboard');
+    } else {
+      toast.error('Invalid credentials');
+    }
+  };
+
+  return (
+    <div className="h-full w-full flex justify-center items-center">
+      <Toaster position="bottom-right"/>
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle>Login</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={handleLogin}>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="username">Username</Label>
+                <Input 
+                  id="username" 
+                  placeholder="Enter username" 
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="password">Password</Label>
+                <Input 
+                  id="password" 
+                  placeholder="Enter password" 
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+              </div>
+            </div>
+            <CardFooter className="flex justify-between mt-4">
+              <Button type="submit">Login</Button>
+            </CardFooter>
+            <p>Don't have an account? 
+              <Link to='/register'> Register</Link>
+            </p>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default Login;
+
